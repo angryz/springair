3b4b7f628a4c0a7319afef2a11ee2a054310417a
angryz <angryzorro@gmail.com>
2016-08-10 17:39:37 +0800

    Remove useless codes from provider demo

--- rpc/rpc-provider-demo/src/main/java/info/noconfuse/springair/rpc/demo/provider/ProviderApplication.java
+++ rpc/rpc-provider-demo/src/main/java/info/noconfuse/springair/rpc/demo/provider/ProviderApplication.java
@@ -1,10 +1,8 @@
 package info.noconfuse.springair.rpc.demo.provider;
 
-import info.noconfuse.springair.rpc.provider.AutoHttpInvokerServicesExporter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.remoting.httpinvoker.HttpInvokerServiceExporter;
 
 /**
  * Created by zzp on 7/29/16.
@@ -14,13 +12,5 @@ public class ProviderApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(ProviderApplication.class, args);
-        String[] beanNames = context.getBeanNamesForType(AutoHttpInvokerServicesExporter.class);
-        for (String beanName : beanNames) {
-            System.out.println(">>> " + beanName);
-        }
-        beanNames = context.getBeanNamesForType(HttpInvokerServiceExporter.class);
-        for (String beanName : beanNames) {
-            System.out.println(">>> " + beanName);
-        }
     }
 }
--- rpc/rpc-provider-demo/src/main/java/info/noconfuse/springair/rpc/demo/provider/RemotingConfiguration.java
+++ rpc/rpc-provider-demo/src/main/java/info/noconfuse/springair/rpc/demo/provider/RemotingConfiguration.java
@@ -12,32 +12,7 @@ import org.springframework.context.annotation.Configuration;
  * Created by zzp on 7/29/16.
  */
 @Configuration
-public class RemotingConfiguration implements ApplicationContextAware {
-
-    private ApplicationContext context;
-
-    /*
-    @Bean(name = "/UserService")
-    HttpInvokerServiceExporter httpInvokerServiceExporter() {
-        String[] beanNames = context.getBeanNamesForAnnotation(RpcService.class);
-        for (String beanName : beanNames) {
-            System.out.println(">>> " + beanName);
-        }
-        Map<String, Object> beanMap = context.getBeansWithAnnotation(RpcService.class);
-        for (Map.Entry<String, Object> entry : beanMap.entrySet()) {
-            System.out.println(">>> " + entry.getKey() + " : " + entry.getValue());
-            Class[] classes = entry.getValue().getClass().getInterfaces();
-            System.out.println("--- " + classes[0].getName());
-            System.out.println("--- " + classes[0].getCanonicalName());
-            System.out.println("--- " + classes[0].getTypeName());
-            System.out.println("--- " + classes[0].getSimpleName());
-        }
-        HttpInvokerServiceExporter exporter = new HttpInvokerServiceExporter();
-        exporter.setService(beanMap.get("userServiceImpl"));
-        exporter.setServiceInterface(UserService.class);
-        return exporter;
-    }
-    */
+public class RemotingConfiguration {
 
     @Bean
     ZookeeperServiceRegistration zookeeperServiceRegistration() {
@@ -46,18 +21,7 @@ public class RemotingConfiguration implements ApplicationContextAware {
 
     @Bean
     AutoHttpInvokerServicesExporter autoHttpInvokerServicesExporter(ZookeeperServiceRegistration serviceRegistration) {
-        //ServiceRegistration serviceRegistration = new ServiceRegistration() {
-        //    @Override
-        //    public void registerService(String serviceName) {
-        //        System.out.println(">>> http://" + LocalHostUtils.ip() + ":" + LocalHostUtils.serverPort() + serviceName);
-        //    }
-        //};
         AutoHttpInvokerServicesExporter exporter = new AutoHttpInvokerServicesExporter(serviceRegistration);
         return exporter;
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.context = applicationContext;
     }
 }
